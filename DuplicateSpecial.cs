972983f05b88012718c77603f49f17ff6ac1cf74
Artyom <artyom@mobit.am>
2019-04-27 00:34:02 +0400

    Add: Rotation

--- DuplicateSpecial.cs
+++ DuplicateSpecial.cs
@@ -4,84 +4,90 @@ using UnityEditor;
 [ExecuteInEditMode]
 public class DuplicateSpecial : EditorWindow
 {
-    int count;
-    Vector3 relOffset;
-    Vector3 conOffset;
+	int count;
+	Vector3 relOffset;
+	Vector3 conOffset;
 
-    // object to duplicate
-    GameObject target;
-    Renderer rend;
+	// object to duplicate
+	GameObject target;
+	Renderer rend;
 
-    // wrapper object for duplicates
-    GameObject wrapper;
+	// wrapper object for duplicates
+	GameObject wrapper;
 
-    [MenuItem("Plugins/Duplicate special")]
-    static void Init()
-    {
-        DuplicateSpecial window = (DuplicateSpecial)GetWindow(typeof(DuplicateSpecial), true, "Duplicate special");
-        window.Show();
-    }
+	[MenuItem("Plugins/Duplicate special")]
+	static void Init()
+	{
+		DuplicateSpecial window = (DuplicateSpecial)GetWindow(typeof(DuplicateSpecial), true, "Duplicate special");
+		window.Show();
+	}
+	Vector3 relRotOffset;
+	void OnGUI()
+	{
+		target = Selection.activeGameObject;
+		if (target == null)
+		{
+			ErrorMsg("Please select an object to duplicate from Hierarchy View");
+			return;
+		}
 
-    void OnGUI()
-    {
-        target = Selection.activeGameObject;
-        if (target == null)
-        {
-            ErrorMsg("Please select an object to duplicate from Hierarchy View");
-            return;
-        }
+		rend = target.GetComponentInChildren<Renderer>();
 
-        rend = target.GetComponentInChildren<Renderer>();
+		EditorGUI.BeginChangeCheck();
 
-        EditorGUI.BeginChangeCheck();
+		count = EditorGUILayout.IntField("Count", count);
+		relOffset = EditorGUILayout.Vector3Field("Relative offset", relOffset);
+		conOffset = EditorGUILayout.Vector3Field("Constant offset", conOffset);
 
-        count = EditorGUILayout.IntField("Count", count);
-        relOffset = EditorGUILayout.Vector3Field("Relative offset", relOffset);
-        conOffset = EditorGUILayout.Vector3Field("Constant offset", conOffset);
+		relRotOffset = EditorGUILayout.Vector3Field("Relative Rotation offset", relRotOffset);
 
-        if (EditorGUI.EndChangeCheck())
-        {
-            Rollback(); // destroy wrapper from previous changes check tick before creating a new one
-            Apply();
-        }
 
-        if (GUILayout.Button("Ok")) Close();
-        if (GUILayout.Button("Cancel"))
-        {
-            Rollback();
-            Close();
-        }
-    }
+		if (EditorGUI.EndChangeCheck())
+		{
+			Rollback(); // destroy wrapper from previous changes check tick before creating a new one
+			Apply();
+		}
 
-    private void Apply()
-    {
-        wrapper = new GameObject(target.name + "_duplicates_" + count);
 
-        for (int i = 1; i <= count; i++)
-        {
-            GameObject clone = Instantiate(target, target.transform.position, target.transform.rotation) as GameObject;
+		if (GUILayout.Button("Reset"))
+		{
+			Rollback();
+			relRotOffset = Vector3.zero;
+			// Close();
+		}
+		if (GUILayout.Button("Apply & Close")) Close();
+	}
 
-            Vector3 offset = new Vector3(rend.bounds.size.x * relOffset.x + conOffset.x,
-                                        rend.bounds.size.y * relOffset.y + conOffset.y,
-                                        rend.bounds.size.z * relOffset.z + conOffset.z)
-                                        * i;
-            clone.transform.Translate(offset);
-            clone.transform.SetParent(wrapper.transform);
-        }
-    }
+	private void Apply()
+	{
+		wrapper = new GameObject(target.name + "_duplicates_" + count);
 
-    private void Rollback()
-    {
-        DestroyImmediate(wrapper);
-    }
+		for (int i = 1; i <= count; i++)
+		{
+			GameObject clone = Instantiate(target, target.transform.position, target.transform.rotation) as GameObject;
 
-    private void ErrorMsg(string msg)
-    {
-        EditorGUILayout.LabelField(msg);
-    }
+			Vector3 offset = new Vector3(rend.bounds.size.x * relOffset.x + conOffset.x,
+										rend.bounds.size.y * relOffset.y + conOffset.y,
+										rend.bounds.size.z * relOffset.z + conOffset.z)
+										* i;
+			clone.transform.Translate(offset);
+			clone.transform.Rotate(clone.transform.rotation.eulerAngles + relRotOffset * i);
+			clone.transform.SetParent(wrapper.transform);
+		}
+	}
 
-    void OnDestroy()
-    {
-        if (count == 0) Rollback();
-    }
+	private void Rollback()
+	{
+		DestroyImmediate(wrapper);
+	}
+
+	private void ErrorMsg(string msg)
+	{
+		EditorGUILayout.LabelField(msg);
+	}
+
+	void OnDestroy()
+	{
+		if (count == 0) Rollback();
+	}
 }
